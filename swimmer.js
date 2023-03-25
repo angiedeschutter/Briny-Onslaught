d8f0e7ca7b6269ac6fdf323f1b6194eb90b3e368
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-03-25 11:16:36 -0700

    added CSS and score table

--- swimmer.js
+++ swimmer.js
@@ -1,15 +1,10 @@
-
-    let swimmer = document.createElement('img')
-    swimmer.src = './assets/Swim1.png'
-    swimmer.style.position = 'absolute'
-    document.body.append(swimmer)
+let swimmer = document.createElement('img')
+swimmer.src = './assets/Swim1.png'
+swimmer.style.position = 'absolute'
+document.body.append(swimmer)
 x=0
+y=0
 
 
-function swimRight(){
-   
-    x=10+x
-    swimmer.style.left = x +'px'
-}
 
-setInterval(swimRight,100
+
